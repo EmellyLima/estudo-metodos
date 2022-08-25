2a7e748923abed3bc001b223bce40d42dc1ccfd4
Emelly Lima <Emellylima.017@gmail.com>
2022-08-25 01:42:39 -0300

    Exercicio

---
+++ Exceçoes/Entities/Exception/DomainAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exceçoes.Entities;
+
+namespace Exceçoes.Entities.Excepition
+{
+    internal class DomainAccount : ApplicationException
+    {
+        public DomainAccount (string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Exceçoes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Exceçoes.Entities;
+using Exceçoes.Entities.Excepition;
+
+namespace Exceçoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.WriteLine("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+
+                Console.WriteLine();
+                Console.WriteLine("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(amount);
+                Console.WriteLine("New balance: " + account);
+            }
+            catch(DomainAccount e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
